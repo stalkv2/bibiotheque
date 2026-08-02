4f0cb937e854d1e85cb089f6b180c28a5f1afa0c
stalk55 <michelateufack71@gmail.com>
2026-08-02 23:18:34 +0100

    creation des entités

---
+++ src/main/java/com/dimsoft/bibliotheque_api/model/Livre.java
@@ -0,0 +1,81 @@
+package com.dimsoft.bibliotheque_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "livres")
+public class Livre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titre;
+
+    @Column(nullable = false)
+    private String auteur;
+
+    private String isbn;
+
+    private boolean disponible = true;
+
+    
+
+    protected Livre() {
+    }
+
+    public Livre(String titre, String auteur, String isbn) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    
+
+}
