88eb9bf3b39309f4745a154d3fda21ece1b1f647
stalk55 <michelateufack71@gmail.com>
2026-08-02 23:01:00 +0100

    initial commit

---
+++ src/main/java/com/dimsoft/bibliotheque_api/BibliothequeApiApplication.java
@@ -0,0 +1,13 @@
+package com.dimsoft.bibliotheque_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeApiApplication.class, args);
+	}
+
+}
